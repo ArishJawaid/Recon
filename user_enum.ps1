cde7ab5cade62aa7704867181917c7a5835a9d0f
ArishJawaid <arish_2000@live.com>
2021-10-07 19:46:36 +0500

    Update user_enum.ps1

--- user_enum.ps1
+++ user_enum.ps1
@@ -1,3 +1,3 @@
 Get-ADUser -Filter * > C:\Users.txt
-whoami >> C:/Windows/Temp/test.txt
-Get-ChildItem C:\ -Filter *.jpg -Recurse -ErrorAction SilentlyContinue | % { $_.FullName } >> C:/Windows/Temp/check.txt
+whoami >> C:\Windows\Temp\test.txt
+Get-ChildItem C:\ -Filter *.jpg -Recurse -ErrorAction SilentlyContinue | % { $_.FullName } >> C:\Windows\Temp\check.txt
